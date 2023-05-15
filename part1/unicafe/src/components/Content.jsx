1c9d2c0d4f19b442eca9aa769cae7c99f51f3bc0
yehoki <patsenn2000@gmail.com>
2023-05-15 19:49:10 +0100

    Ex 1.7, 1.8, 1.9

--- part1/unicafe/src/components/Content.jsx
+++ part1/unicafe/src/components/Content.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import { Score } from "./Score";
 
 export const Content = (props) => {
-  return (
-    <div>
-      <Score text="Good" score={props.good} />
-      <Score text="Neutral" score={props.neutral} />
-      <Score text="Bad" score={props.bad} />
-    </div>
-  );
+  const total = props.good + props.bad + props.neutral;
+  const average = total === 0 ? 0 : (props.good - props.bad) / total;
+  const positive = total === 0 ? 0 : `${(props.good / total) * 100} %`;
+  const display =
+    total === 0 ? (
+      <>No feedback given</>
+    ) : (
+      <>
+        <Score text="Good" score={props.good} />
+        <Score text="Neutral" score={props.neutral} />
+        <Score text="Bad" score={props.bad} />
+        <Score text="All" score={total} />
+        <Score text="Average" score={average} />
+        <Score text="Positive" score={positive} />
+      </>
+    );
+
+  return <div>{display}</div>;
 };
