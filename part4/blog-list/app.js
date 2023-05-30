647b7f3f19018afb57ded55b6ced362cab2a1cf8
yehoki <patsenn2000@gmail.com>
2023-05-30 14:35:41 +0100

    Refactor logger and config

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,42 +1,40 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
-})
+  likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = 'mongodb+srv://testuser:testuser@bookmarkt.vm0t4pg.mongodb.net/bloglist?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl);
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.get("/api/blogs", (request, response) => {
+  logger.info("testing");
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
+});
