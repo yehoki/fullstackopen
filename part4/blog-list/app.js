e362b538f45895e673611eaa7b549d9f3108c0db
yehoki <patsenn2000@gmail.com>
2023-05-30 14:44:31 +0100

    Model refactor

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -4,15 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = require('./models/blog')
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
@@ -21,7 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/api/blogs", (request, response) => {
-  logger.info("testing");
   Blog.find({}).then((blogs) => {
     response.json(blogs);
   });
@@ -29,12 +20,13 @@ app.get("/api/blogs", (request, response) => {
 
 app.post("/api/blogs", (request, response) => {
   const blog = new Blog(request.body);
-
   blog.save().then((result) => {
     response.status(201).json(result);
   });
 });
 
+
 app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
-});
+    logger.info(`Server running on port ${config.PORT}`);
+  });
+
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,4 +1,6 @@
-const app = require('./app');
+const app = require("./app");
+const config = require("./utils/config");
 
-
-app.listen()
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
+});
